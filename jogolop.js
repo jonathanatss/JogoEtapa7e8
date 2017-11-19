81251c55829fbafeef0945fa637ad035c7f8328b
jonathanatss <30415582+jonathanatss@users.noreply.github.com>
2017-11-19 18:23:16 -0200

    Add files via upload

--- jogolop.js
+++ jogolop.js
@@ -2,6 +2,14 @@ var posX, posY; //bola
 var fx = 900;
 var fy = 600;
 
+var velocidade1 = 15;
+var velocidade2 = 17;
+var velocidade3 = 19;
+var velocidade4 = 21;
+var velocidade5 = 23;
+
+var corBranco = true; 
+var trocouCor = false; 
 
 // POSIÇÕES DE TODOS OS QUADRADOS
 
@@ -30,7 +38,7 @@ var bg;
 
 var tamBloco = 90;
 
-var vidas = 3;
+var vidas = 5;
 var pontos = 0;
 
 function preload(){
@@ -64,7 +72,7 @@ function setup() {
 
 function draw() {
  background (0);
-	if(vidas > 0) {	
+	if(vidas == 1) {	
 		//Gerar cenário
 		for(j = 0; j < cenario.length; j++) {
 			for(i = 0; i < cenario.length; i++) {
@@ -76,12 +84,65 @@ function draw() {
 			}
 		}
   }
+  
+  if(vidas == 5) {	
+		//Gerar cenário
+		for(j = 0; j < cenario.length; j++) {
+			for(i = 0; i < cenario.length; i++) {
+				if(cenario[i][j] == "#") {
+					//image(img, j*tamBloco, i*tamBloco);
+					fill(110,232,255);
+					rect(j*tamBloco,i*tamBloco,tamBloco,tamBloco);
+				}
+			}
+		}
+  }
+  
+  if(vidas == 4) {	
+		//Gerar cenário
+		for(j = 0; j < cenario.length; j++) {
+			for(i = 0; i < cenario.length; i++) {
+				if(cenario[i][j] == "#") {
+					//image(img, j*tamBloco, i*tamBloco);
+					fill(300,250,255);
+					rect(j*tamBloco,i*tamBloco,tamBloco,tamBloco);
+				}
+			}
+		}
+  }
+  
+  if(vidas == 3) {	
+		//Gerar cenário
+		for(j = 0; j < cenario.length; j++) {
+			for(i = 0; i < cenario.length; i++) {
+				if(cenario[i][j] == "#") {
+					//image(img, j*tamBloco, i*tamBloco);
+					fill(234,110,255);
+					rect(j*tamBloco,i*tamBloco,tamBloco,tamBloco);
+				}
+			}
+		}
+  }
+  
+  if(vidas == 2) {	
+		//Gerar cenário
+		for(j = 0; j < cenario.length; j++) {
+			for(i = 0; i < cenario.length; i++) {
+				if(cenario[i][j] == "#") {
+					//image(img, j*tamBloco, i*tamBloco);
+					fill(180,10,255);
+					rect(j*tamBloco,i*tamBloco,tamBloco,tamBloco);
+				}
+			}
+		}
+  }
    
   //função para o formato das estrelas  
   function star(x, y, radius1, radius2, npoints) {
   var angle = TWO_PI / npoints;
   var halfAngle = angle/2.0;
   beginShape();
+ 
   for (var a = 0; a < TWO_PI; a += angle) {
     var sx = x + cos(a) * radius2;
     var sy = y + sin(a) * radius2;
@@ -95,7 +156,7 @@ function draw() {
  
   textSize(14); // define o tamanho da fonte
   fill(255); 
-  //text(contagem + " bolas já se passaram", 350, 80); // escreve na tela
+  
  
   
   //movimentos da bola
@@ -130,12 +191,10 @@ function draw() {
 			posY = 90;  
 		}
 
-ellipse(posX, posY, 40, 40);//bola 
-  
  	
 	//Movimtos quadrado 1
 	if (y1 < 900){
-		y1= y1 + random(0,13);
+		y1= y1 + random(0,velocidade1);
 			}
 	else{
 		y1 = 0;
@@ -144,7 +203,7 @@ ellipse(posX, posY, 40, 40);//bola
 	
 	//Movimentos quadrado 2
 	if (y2 < 900){
-		y2= y2 + random(0,14);
+		y2= y2 + random(0,velocidade2);
 			}
 	else{
 		y2 = 0;
@@ -153,7 +212,7 @@ ellipse(posX, posY, 40, 40);//bola
 	
 	//Movimentos quadrado 3
 	if (y3 < 900){
-		y3= y3 + random(0,15);
+		y3= y3 + random(0,velocidade3);
 			}
 	else{
 		y3 = 0;
@@ -162,7 +221,7 @@ ellipse(posX, posY, 40, 40);//bola
 	
 	//Movimentos quadrado 4
 	if (y4 < 900){
-		y4= y4 + random(0,16);
+		y4= y4 + random(0,velocidade4);
 			}
 	else{
 		y4 = 0;
@@ -171,51 +230,158 @@ ellipse(posX, posY, 40, 40);//bola
 		 
 	//Movimentos quadrado 5
 	if (y5 < 900){
-		y5= y5 + random(0,17);
+		y5= y5 + random(0,velocidade5);
 			}
 	else{
 		y5 = 0;
 	} 
 	star(x5, y5, 20, 10, 5); //quadrado5
 	
-			
+	
+	//Menu
+	textSize(60);
+	text("Pontos: "+pontos, 500, 50);
+	
+	//Colisões			
 	if(dist(posX,posY,x1,y1) <= 35 && dist(posX,posY,x1,y1) >= 31) {
-		//alert("Nave destruída!");
 		vidas--;
-	   	}
+		velocidade1+= 5;
+		velocidade2+= 5;
+		velocidade3+= 5;
+		velocidade4+= 5;
+		velocidade5+= 5;
+		//Troca cor
+		if ( trocouCor == false) { 
+			corBranco = ! corBranco;
+			trocouCor = true; 
+		}
+ 	}
+	else {
+		trocouCor = false;  
+   }
+	if ( corBranco ) {
+		fill(255);
+ }
+	else {
+		fill(255,0,0);
+ }
+		
+	ellipse(posX, posY, 40, 40);//bola 
+	   	
 	if(dist(posX,posY,x2,y2) <= 35 && dist(posX,posY,x2,y2) >= 31) {
-		//alert("Nave destrúda!");
 		vidas--;
-	 }
+		velocidade1+= 5;
+		velocidade2+= 5;
+		velocidade3+= 5;
+		velocidade4+= 5;
+		velocidade5+= 5;
+		//Troca cor
+		if ( trocouCor == false) { 
+			corBranco = ! corBranco;
+			trocouCor = true; 
+		}
+ 	}
+	else {
+		trocouCor = false;  
+   }
+	if ( corBranco ) {
+		fill(255);
+ }
+	else {
+		fill(255,0,0);
+ }
+		
+	ellipse(posX, posY, 40, 40);//bola 
+	 
 	if(dist(posX,posY,x3,y3) <= 35 && dist(posX,posY,x3,y3) >= 31) {
-		//alert("Nave destruída!");
 		vidas--;
-	   	}
+		velocidade1+= 5;
+		velocidade2+= 5;
+		velocidade3+= 5;
+		velocidade4+= 5;
+		velocidade5+= 5;
+		if ( trocouCor == false) { 
+			corBranco = ! corBranco;
+			trocouCor = true; 
+		}
+ 	}
+	else {
+		trocouCor = false;  
+   }
+	if ( corBranco ) {
+		fill(255);
+ }
+	else {
+		fill(255,0,0);
+ }
+		
+	ellipse(posX, posY, 40, 40);//bola 
+	   	
 	  	
 	if(dist(posX,posY,x4,y4) <= 35 && dist(posX,posY,x4,y4) >= 31) {
-		//alert("Nave destruída!");
 		vidas--;
-	   	}
+		velocidade1+= 5;
+		velocidade2+= 5;
+		velocidade3+= 5;
+		velocidade4+= 5;
+		velocidade5+= 5;
+		if ( trocouCor == false) { 
+			corBranco = ! corBranco;
+			trocouCor = true; 
+		}
+ 	}
+	else {
+		trocouCor = false;  
+   }
+	if ( corBranco ) {
+		fill(255);
+ }
+	else {
+		fill(255,0,0);
+ }
+		
+	ellipse(posX, posY, 40, 40);//bola 
+		
 	
 	if(dist(posX,posY,x5,y5) <= 35 && dist(posX,posY,x5,y5) >= 31) {
-		//alert("Nave destruída!");
 		vidas--;
-	}
+		velocidade1+= 5;
+		velocidade2+= 5;
+		velocidade3+= 5;
+		velocidade4+= 5;
+		velocidade5+= 5;
+		if ( trocouCor == false) { 
+			corBranco = ! corBranco;
+			trocouCor = true; 
+		}
+ 	}
+	else {
+		trocouCor = false;  
+   }
+	if ( corBranco ) {
+		fill(255);
+ }
+	else {
+		fill(255,0,0);
+ }
+		
+	ellipse(posX, posY, 40, 40);//bola 
+	
+	
 	if (vidas == 0){
-			y1=-30;
-			y2=-30;
-			y3=-30;
-			y4=-30;
-			y5=-30;
+			y1=-500;
+			y2=-500;
+			y3=-500;
+			y4=-500;
+			y5=-500;
 			vidas=0;
 	   	}
 	
 	textSize(60);
 	text("Vidas: " + vidas, 50, 50);
-	text("Pontos: "+pontos, 500, 50);
-	
+		
 	if (vidas <= 0){
-		text("Game Over", 340, 300);
+		text("Game Over", 320, 300);
 	    
 	
 	}
